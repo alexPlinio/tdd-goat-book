1f15ad6ed1a7460d5401ef13aad6f3dc0f1af5e6
alex.guglielmetti@berkeley.edu <alex.guglielmetti@berkeley.edu>
2021-04-22 14:43:02 -0400

    First Functional Testing specced out in comments, and now uses unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,38 @@
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Check out user homepage
+        self.browser.get('http://localhost:8000')
+        #Check page title and header mention to-do list
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test!')
+
+#The line below is used by python to run the script directly
+#but not run it if the file is imported.
+if __name__ == '__main__':
+    #It then imports this modules, examines it, gets a list of all classes and functions which could
+    #be tests (according the configuration) and then creates a test case for each of them.
+    unittest.main(warnings='ignore')
+
+
+
+
+
+
+
+
+
+#browser = webdriver.Firefox()
+#browser.get('http://localhost:8000')
+#assert 'To-Do' in browser.title, "Browser title was " + browser.title
+#browser.quit()
