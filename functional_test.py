a2c8b264bb8b7528e91e6eaebfb7ea92ee28a4d4
alex.guglielmetti@berkeley.edu <30442611+alexPlinio@users.noreply.github.com>
2021-04-24 14:08:56 -0400

    Front page HTML now genereated from a template

--- functional_test.py
+++ functional_test.py
@@ -32,7 +32,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows),
+                "New to-do item did not appear on the table")
 
         self.fail('Finish The Test')
         
