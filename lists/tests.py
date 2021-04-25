a961ad2abb9fa00d45f33cfe34a35cf48e5018fc
alex.guglielmetti@berkeley.edu <30442611+alexPlinio@users.noreply.github.com>
2021-04-24 20:58:54 -0400

    Interim commit

--- lists/tests.py
+++ lists/tests.py
@@ -7,32 +7,15 @@
 # internal function to resolve URLS and find what view function they should map to
 #from django.urls import resolve #DELETE
 from django.test import TestCase
-#from django.http import HttpRequest #DELETE
-#from django.template.loader import render_to_string #DELETE
-
-#from lists.views import home_page #DELETE
-
 
 
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
-        print('>>> RESPONSE: ', response)
         self.assertTemplateUsed(response, 'home.html')
 
-    #    def test_root_url_resolves_to_home_page_view(self):
-#        found = resolve('/')
-#        self.assertEqual(found.func,home_page)
-
-
-#    def test_home_page_returns_correct_html(self):
-        #request = HttpRequest()
-        #response = home_page(request)
-#        response = self.client.get('/')
-#        html = response.content.decode('utf8')
-#        self.assertTrue(html.startswith('<html>'))
-#        self.assertIn('<title>To-Do lists</title>',html)
-#        self.assertTrue(html.strip().endswith('</html>'))
-
-#        self.assertTemplateUsed(response, 'wrong.html')
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data = {'item_text' : 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,17 @@
 #====================================
 
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')
+    return render(request, 'home.html', {
+        'new_item_text':request.POST.get('item_text'),
+        })
 
+#    if request.method == 'POST': #DELETE
+#        return HttpResponse (request.POST['item_text']) #DELETE
+#    return render(request,'home.html') #DELETE
+    
 
 
