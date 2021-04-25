806db930ad9ea27533a92842a4e4d4a9591dcb21
alex.guglielmetti@berkeley.edu <30442611+alexPlinio@users.noreply.github.com>
2021-04-25 19:08:58 -0400

    Minor update functional_test.py update

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Check out user homepage
         self.browser.get('http://localhost:8000')
@@ -22,23 +27,33 @@ class NewVisitorTest(unittest.TestCase):
         #Location 1062 input
         header_text =  self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
+
+        #When she hits enter, the page updates, and now the page lists 
+        #"1:Buy peacock feathers" ...
+        #inputbox.send_keys(Keys.ENTER)
+        #time.sleep(1)
+        #self.check_for_row_in_list_table('1:Buy peacock feathers')
         
+        #There is still a text box inviting her to add another item...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
-        #self.assertEqual(inputbox.get_attribute('placeholder'),
-        #                'Enter a to-do item')
-        #inputbox.send_keys('Buy peacock feathers')
-        #inputbox.send_keys(Keys.ENTER)
-        #time.sleep(1)
+
 
         #The page updates again...
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        #self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
+        
+        #Edith wonders...
+
 
         self.fail('Finish The Test')
         
