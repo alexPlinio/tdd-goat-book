86dc9023610f2e0679fcc1fc2adf961007140240
alex.guglielmetti@berkeley.edu <30442611+alexPlinio@users.noreply.github.com>
2021-04-28 18:50:41 -0400

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,8 +17,6 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
- 
-
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -32,7 +30,33 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    #def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_layout_and_styling(self):
+        #Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width']/2,
+                512,
+                delta = 10
+                )
+
+
+        #She starts a new list and sees the input nicely
+        #centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width']/2,
+                512,
+                delta = 10
+                )
+
+
     def test_can_start_a_list_for_one_user(self):
         #Edith has heard about a cool...
         self.browser.get(self.live_server_url)
