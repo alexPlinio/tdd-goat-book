5ac0a0571a286baf1a31c0f49288b1114a33cd1f
alex.guglielmetti@berkeley.edu <30442611+alexPlinio@users.noreply.github.com>
2021-05-01 19:24:19 -0400

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -15,17 +15,26 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '28pyd4f9vi+8wsi+#p&ua7zdax+p34qy#&7z3orf@zmjnd26ay'
+#SECRET_KEY = '28pyd4f9vi+8wsi+#p&ua7zdax+p34qy#&7z3orf@zmjnd26ay'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = ['*'] # Hack to make staging server run
+#ALLOWED_HOSTS = ['*'] # Hack to make staging server run
 
 
 # Application definition
